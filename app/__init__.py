5118938be3d5117fc5fee38fc76d5ceeb1a374ea
Max Taylor <maxbransontaylor@gmail.com>
2023-01-16 22:29:08 -0500

    optimized db connection for flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
 
 
 def create_app(test_config=None):
@@ -12,4 +13,5 @@ def create_app(test_config=None):
 
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    init_db()
     return app
